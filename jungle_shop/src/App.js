08a9b1e539434c83a9c6bd2a9783b65f352813b0
hyperbola88 <>
2022-05-20 18:50:20 +0300

    first commit

---
+++ jungle_shop/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href='/'>Jungle Shop</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All rights reserved by Me
+      </footer>
+    </div>
+  );
+}
+
+export default App;
